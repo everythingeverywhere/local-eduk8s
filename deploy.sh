4b32aea0ce44cffea8ecb390ad8ffe0a8082cd0c
everythingeverywhere <ivan0tarin@gmail.com>
2020-07-01 13:16:51 -0600

    git init

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+# Create local cluster
+minikube start --kubernetes-version=v1.17.8 && \
+
+# Enable the NGINX Ingress controller
+minikube addons enable ingress && \
+
+# Retrieve local ip for ingress_domain
+MINIKUBE_IP=$(minikube ip) && \
+
+# Deploying the eduk8s operator
+kubectl apply -k "github.com/eduk8s/eduk8s?ref=master" && \
+
+# Specifying the ingress domain
+# Running locally and you don’t have a custom domain name which maps to the IP for the cluster, you can use a nip.io address
+kubectl set env deployment/eduk8s-operator -n eduk8s INGRESS_DOMAIN=${MINIKUBE_IP}.nip.io && \
+
+# Create Training Session
+
+# To load the definition of the WORKSHOP, run:
+kubectl apply -f https://raw.githubusercontent.com/eduk8s-labs/lab-k8s-fundamentals/master/resources/workshop.yaml && \
+
+
+# Creating the workshop TRAINING PORTAL
+kubectl apply -f https://raw.githubusercontent.com/eduk8s-labs/lab-k8s-fundamentals/master/resources/training-portal.yaml
+
+# To create the workshop environment run: 
+kubectl apply -f https://raw.githubusercontent.com/eduk8s-labs/lab-k8s-fundamentals/master/resources/workshop-environment.yaml
+
---
+++ new-user.sh
@@ -0,0 +1,1 @@
+curl -v -H "Authorization: Bearer <access-token>" http://lab-k8s-fundamentals-ui.192.168.64.30.nip.io.test/workshops/environment/<name>/request/?index_url=https://hub.test/&user=<user>
